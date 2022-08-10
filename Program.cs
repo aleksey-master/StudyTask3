2df86295bc3258fe9768b651cace6574e844ae58
Aleksey <Aleksey-master@yandex.ru>
2022-08-10 19:58:23 +0400

    Study2Task2

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ int number = Convert.ToInt32(Console.ReadLine());
 string numberText = Convert.ToString(number);
 if (numberText.Length > 2)
 {
-    Console.WriteLine("третья цифра -> " + numberText[2]);
+    Console.WriteLine("третья цифра: " + numberText[2]);
 }
 else
 {
-    Console.WriteLine("-> третьей цифры нет");
+    Console.WriteLine("Tретьей цифры нет");
 }
 
 
